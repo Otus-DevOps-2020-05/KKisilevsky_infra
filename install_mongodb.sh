8d679565f052276411b6aabd8bbaaad966efd121
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2020-06-22 22:48:58 +0200

    Fix error in script install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt-get update
+apt-get install -y apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/mongodb-org-4.2.list
 #sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list
