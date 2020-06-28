53af1758206d8562ff354b415b0a9d7cc7f89c86
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2020-06-28 11:42:21 +0200

    Add script for build image reddit-full

--- packer/files/build_and_run.sh
+++ packer/files/build_and_run.sh
@@ -2,7 +2,16 @@
 apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
+echo "[Unit]" >> service
+echo "Description=Puma" >> service
+echo " " >> service
+echo "[Service]" >> service
+echo "ExecStart=/usr/local/bin/puma -d" >> service
+echo " " >> service
+echo "[Install]" >> service
+echo "WantedBy=multi-user.target" >> service
 touch /etc/systemd/system/puma.service
+cat service > /etc/systemd/system/puma.service
 chmod 664 /etc/systemd/system/puma.service
 systemctl daemon-reload
 systemctl start puma
