6421ef8055e02e10fae9c214b2be64b4fb60847f
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2020-07-08 22:51:50 +0200

    Terraform-2 homework completed

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,11 @@
+variable app_disk_image {
+  description = "Disk image for ruby application"
+  default     = "reddit-app-base"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable subnet_id {
+  description = "Subnet for modules"
+}
