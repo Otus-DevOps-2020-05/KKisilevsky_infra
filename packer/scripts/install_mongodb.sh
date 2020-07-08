7e022937e4776ebe2717e6ac8b4e72e8a3364975
Konstantin Kisilevskiy <xlonelysoul94@gmail.com>
2020-07-08 22:58:47 +0200

    Added new packer files

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add - # После пайпа sudo не убрано - а то не отработает добавление ключа
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-apt-get update 
+apt-get update
+rm /var/lib/apt/lists/lock
+rm /var/cache/apt/archives/lock
+rm /var/lib/dpkg/lock
+rm /var/lib/dpkg/lock-frontend 
 apt-get install -y mongodb-org
 systemctl start mongod
 systemctl enable mongod
